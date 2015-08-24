238c9fb2cd7420637b34582f8bb70369ddb1ffbb
patte <patrick.recher@gmail.com>
2015-08-24 14:30:08 +0200

    add mobile-config.js to configure generated ios bundle

---
+++ app/mobile-config.js
@@ -0,0 +1,10 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  id: 'org.patpat.epiph',
+  name: 'epiph',
+  description: 'clinical trial with epiphanies',
+  author: 'Patrick Recher',
+  email: 'patrick.recher@gmail.com',
+  website: 'https://epiph.patpat.org'
+});
